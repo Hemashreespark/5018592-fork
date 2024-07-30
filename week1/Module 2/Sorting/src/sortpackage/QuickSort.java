a8a87b378a181e815ef1293e0153ccc11ded467f
hemashreesparkk <hemashreesparkk@outlook.com>
2024-07-30 15:17:28 +0530

    ecom

--- week1/Module 2/Sorting/src/sortpackage/QuickSort.java
+++ week1/Module 2/Sorting/src/sortpackage/QuickSort.java
@@ -12,19 +12,18 @@ public class QuickSort {
 
     private int partition(Order[] orders, int low, int high) {
         double pivot = orders[high].totalPrice;
-        int i = (low - 1); // Index of smaller element
+        int i = (low - 1);
         for (int j = low; j < high; j++) {
             if (orders[j].totalPrice < pivot) {
                 i++;
 
-                // Swap orders[i] and orders[j]
+                
                 Order temp = orders[i];
                 orders[i] = orders[j];
                 orders[j] = temp;
             }
         }
 
-        // Swap orders[i+1] and orders[high] (or pivot)
         Order temp = orders[i + 1];
         orders[i + 1] = orders[high];
         orders[high] = temp;
